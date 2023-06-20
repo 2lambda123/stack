2fb6b32f27b2c56159baec6bd0be820b948b6d93
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2023-06-20 22:23:16 +0100

    Update integration test 620-env-command

--- test/integration/tests/620-env-command/files/run.sh
+++ test/integration/tests/620-env-command/files/run.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-stack build --resolver lts-20.23 async
-eval `stack config env --resolver lts-20.23`
+stack build --resolver nightly-2023-06-18 async
+eval `stack config env --resolver nightly-2023-06-18`
 ghc Main.hs
